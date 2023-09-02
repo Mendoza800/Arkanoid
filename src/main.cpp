684738c2599d4a21835917c5ba22a1343369b20d
Emiliano Mendoza Achirica <143230165+Mendoza800@users.noreply.github.com>
2023-09-02 00:45:16 +0000

    Creamos mascota

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <Mascota.hpp>
 
 int main()
 {
-    std::cout<<"Hola Mundo"<<std::endl;
+    std::cout<<"Juego de mascotas"<<std::endl;
+
+    Mascota m1,m2,m3;
+
+    m1.EstablecerNombre("Pi");
+    m2.EstablecerNombre("ti");
+    m3.EstablecerNombre("to");
+
+    m1.DecirNombre();
+    m2.DecirNombre();
+    m3.DecirNombre();
+
     return 0;
 }
