a846c50d42a1ea76d4606e4b2c879decdc9749cb
Emiliano Mendoza Achirica <143230165+Mendoza800@users.noreply.github.com>
2023-09-11 01:39:34 +0000

    P

--- src/arkanoid.cpp
+++ src/arkanoid.cpp
@@ -1,1 +1,54 @@
+#include <iostream>
+#include <Color.hpp>
+#include <Verde.hpp>
+#include <Rojo.hpp>
+#include <Blanco.hpp>
+#include <Amarillo.hpp>
+#include <Naranja.hpp>
+#include <Bolita.hpp>
+#include <Barra.hpp>
+#include <Vida.hpp>
+#include <Jugador.hpp>
+#include <Borde.hpp>
+#include <Fondo.hpp>
+#include <Puntaje.hpp>
+#include <Ronda.hpp>
+#include <Pantalla.hpp>
+#include <Bloque.hpp>
+#include <Limite.hpp>
+#include <Poder.hpp>
+#include <BarraChica.hpp>
+#include <BarraGrande.hpp>
+#include <BolaDestructora.hpp>
+#include <MultiBola.hpp>
+#include <Eliminador.hpp>
 
+int main ()
+{
+    std::cout<<"ARKANOID"<<std::endl;
+    Color c;
+    Verde v;
+    Rojo r;
+    Blanco b;
+    Naranja n;
+    Amarillo a;
+    Bolita bl;
+    Barra br;
+    Vida vid;
+    Jugador plyr;
+    Borde brd;
+    Fondo fnd;
+    Puntaje pntj;
+    Ronda rnd;
+    Pantalla pntll;
+    Bloque blq;
+    Limite lmt;
+    Poder pdr;
+    BarraChica brch;
+    BarraGrande brgrnd;
+    BolaDestructora bldstr;
+    MultiBola mltbl;
+    Eliminador elmndr;
+
+    return 0;
+}
